36f33ecddd392e292fc2852b9be0b7e92cbe2d7e
Larry Peck <larpek@gmail.com>
2023-03-19 10:39:26 -0400

    Filled in the rest of the plants

--- src/data/plants.js
+++ src/data/plants.js
@@ -1,16 +1,50 @@
 export const plantData = {
     "Large Geranium Deck Pots" : [
         {"name": "All Red", "unit": "Pots", "cost": 2400},
-        {"name": "Hot Mix", "unit": "Pots", "cost": 2400},
-        {"name": "All Orange", "unit": "Pots", "cost": 2400}
+        {"name": "Hot Mix - Bright Orange and Violet", "unit": "Pots", "cost": 2400},
+        {"name": "All Orange", "unit": "Pots", "cost": 2400},
+        {"name": "All White", "unit": "Pots", "cost": 2400},
+        {"name": "Miami Mix - Flamingo Rose & Violet", "unit": "Pots", "cost": 2400},
+        {"name": "All Violet", "unit": "Pots", "cost": 2400}
     ],
     "Premium Hanging Baskets" : [
         {"name": "Boston Fern", "unit": "Baskets", "cost": 1800},
-        {"name": "Streptocarpus", "unit": "Baskets", "cost": 1800},
-        {"Ivy Geranium Hanging Baskets" : [
-            {"name": "Red", "unit": "Baskets", "cost": 1400},
-            {"name": "Lavender Rose", "unit": "Baskets", "cost": 1400},
-            {"name": "Crimson Flame", "unit": "Baskets", "cost": 1400}
-        ]}
+        {"name": "Streptocarpus", "unit": "Baskets", "cost": 1800}
+    ],
+    "Ivy Geranium Hanging Baskets (Calliope)" : [
+        {"name": "Red", "unit": "Baskets", "cost": 1400},
+        {"name": "Lavender Rose", "unit": "Baskets", "cost": 1400},
+        {"name": "Crimson Flame - Bi-color Red/Hot Pink", "unit": "Baskets", "cost": 1400}
+    ],
+    "" : [
+        {"name": "Sunpatiens Hanging Baskets - Magenta", "unit": "Baskets", "cost": 1400}
+    ],
+    "Best Sellers" : [
+        {"name": "Baby Tut Grass", "unit": "Plants", "cost": 700},
+        {"name": "Purple Fountain Grass", "unit": "Plants", "cost": 700},
+        {"name": "Caladium - White with Green Vein", "unit": "Plants", "cost": 600}
+    ],
+    "Geraniums" : [
+        {"name": "Red", "unit": "Plants", "cost": 375},
+        {"name": "White", "unit": "Plants", "cost": 375},
+        {"name": "Pink / Neon Rose", "unit": "Plants", "cost": 375},
+        {"name": "Dark Pink / Strawberry Sizzle", "unit": "Plants", "cost": 375},
+        {"name": "Hot Magenta / Cranberry Sizzle", "unit": "Plants", "cost": 375}
+    ],
+    "Sunpatiens" : [
+        {"name": "Deep Rose", "unit": "Plants", "cost": 450},
+        {"name": "Orange", "unit": "Plants", "cost": 450},
+        {"name": "Magenta", "unit": "Plants", "cost": 450},
+        {"name": "White", "unit": "Plants", "cost": 450}
+    ],
+    "Sweet Potato Vines" : [
+        {"name": "Margarita", "unit": "Plants", "cost": 350},
+        {"name": "Tri-Color", "unit": "Plants", "cost": 350},
+        {"name": "Ace of Spades", "unit": "Plants", "cost": 350}
+    ],
+    " " : [
+        {"name": "Vinca Vine", "unit": "Plants", "cost": 350},
+        {"name": "Creeping Jenny", "unit": "Plants", "cost": 350},
+        {"name": "Spikes", "unit": "Plants", "cost": 350}
     ]
 };
