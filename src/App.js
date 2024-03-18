80860254ac328dc4abc971d429a183badf71bed2
Grady Peck <gradypeck717@gmail.com>
2024-03-18 17:28:46 -0400

    more emailJS data updates

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
           'service_vbj6dhc', 
           'template_cj6ncrr', 
           {from_name: `${firstNameInput.current.value} ${lastNameInput.current.value}`, email_body: `Order Summary: ${summary}`, CSV_content: csv}, 
-          {publicKey: '1Pwmo3BHx-shOTe4M'}
+          '1Pwmo3BHx-shOTe4M'
         )
       );
 
@@ -105,7 +105,7 @@ function App() {
               'service_vbj6dhc', 
               'template_u3adivs', 
               templateParams, 
-              {publicKey: '1Pwmo3BHx-shOTe4M'}
+              '1Pwmo3BHx-shOTe4M'
             )
           );
         },
